e7143447210d47575ac559591ba9e5002a4b7094
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-04-27 13:11:26 +0100

    Tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,46 +10,72 @@ import           Test.DocTest
 import           Numeric.WordOfSize
 import           Numeric.IntOfSize
 import           Numeric.Decimal
+import           Numeric.Natural
 
 import           Data.Word
 import           Data.Int
 
 plusSame
-    :: (Integral a, Integral b)
-    => (Integer -> a) -> (Integer -> b) -> Property
-plusSame toBase toTest = property $ do
+    :: (Integral a, Num b)
+    => (Integer -> a) -> (Integer -> b) -> (b -> Integer) -> Property
+plusSame toBase toTest back = property $ do
     x <- forAll (Gen.integral (Range.linear (-1000) 1000))
     y <- forAll (Gen.integral (Range.linear (-1000) 1000))
     let zb = toBase x + toBase y
     let zt = toTest x + toTest y
-    toInteger zb === toInteger zt
+    toInteger zb === back zt
+
+multSame
+    :: (Integral a, Num b)
+    => (Integer -> a) -> (Integer -> b) -> (b -> Integer) -> Property
+multSame toBase toTest back = property $ do
+    x <- forAll (Gen.integral (Range.linear (-1000) 1000))
+    y <- forAll (Gen.integral (Range.linear (-1000) 1000))
+    let zb = toBase x * toBase y
+    let zt = toTest x * toTest y
+    toInteger zb === back zt
 
 prop_WordPlusSame :: Property
 prop_WordPlusSame =
     plusSame
         ((fromInteger @ Word8) . abs)
         ((fromInteger @ (WordOfSize 8)) . abs)
+        toInteger
 
 prop_IntPlusSame :: Property
 prop_IntPlusSame =
     plusSame
         (fromInteger @ Int8)
         (fromInteger @ (IntOfSize 8))
+        toInteger
 
-prop_sumDig :: Property
-prop_sumDig = property $ do
-    x <- forAll (Gen.integral (Range.linear 0 9))
-    y <- forAll (Gen.integral (Range.linear 0 9))
-    z <- forAll (Gen.integral (Range.linear 0 9))
-    fromEnum (sumDig (toEnum x) (toEnum y) (toEnum z)) === ((x + y + z) `rem` 10)
+prop_DecimalPlusSame :: Property
+prop_DecimalPlusSame =
+    plusSame
+        (fromInteger @ Natural . abs)
+        (fromInteger @ Nat . abs)
+        (foldr (\e a -> toInteger (fromEnum e) + 10 * a) 0 . getNat)
 
-prop_carryDig :: Property
-prop_carryDig = property $ do
-    x <- forAll (Gen.integral (Range.linear 0 9))
-    y <- forAll (Gen.integral (Range.linear 0 9))
-    z <- forAll (Gen.integral (Range.linear 0 9))
-    fromEnum (carryDig (toEnum x) (toEnum y) (toEnum z)) === ((x + y + z) `quot` 10)
+prop_WordMultSame :: Property
+prop_WordMultSame =
+    multSame
+        ((fromInteger @ Word8) . abs)
+        ((fromInteger @ (WordOfSize 8)) . abs)
+        toInteger
 
+prop_IntMultSame :: Property
+prop_IntMultSame =
+    multSame
+        (fromInteger @ Int8)
+        (fromInteger @ (IntOfSize 8))
+        toInteger
+
+prop_DecimalMultSame :: Property
+prop_DecimalMultSame =
+    multSame
+        (fromInteger @ Natural . abs)
+        (fromInteger @ Nat . abs)
+        (foldr (\e a -> toInteger (fromEnum e) + 10 * a) 0 . getNat)
 main :: IO Bool
 main = do
     doctest ["-isrc","src/"]
