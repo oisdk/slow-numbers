e635a03bb7bb63f765bc121c3f1165db949dbb5b
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-01-16 12:17:19 +0000

    added another proof

--- src/Numeric/Peano/Proofs.hs
+++ src/Numeric/Peano/Proofs.hs
@@ -29,14 +29,22 @@ plusZeroNeutral (SS n) = case plusZeroNeutral n of
   Refl -> Refl
 {-# NOINLINE plusZeroNeutral #-}
 
--- | Successor distributes over addition
+-- | Successor distributes over addition.
 plusSuccDistrib :: Sing n -> proxy m -> n :+ S m :~: S (n :+ m)
 plusSuccDistrib SZ _     = Refl
 plusSuccDistrib (SS n) p = gcastWith (plusSuccDistrib n p) Refl
 {-# NOINLINE plusSuccDistrib #-}
 
+-- | Addition is commutative.
+plusComm :: Sing (n :: Peano) -> Sing (m :: Peano) -> (n :+ m) :~: (m :+ n)
+plusComm SZ m = gcastWith (plusZeroNeutral m) Refl
+plusComm (SS n) m =
+    gcastWith (plusSuccDistrib m n) (gcastWith (plusComm n m) Refl)
+{-# NOINLINE plusComm #-}
+
 {-# RULES
-"plusAssoc" forall x y z. plusAssoc x y z = unsafeCoerce (Refl :: Z :~: Z)
-"plusZeroNeutral" forall x. plusZeroNeutral x = unsafeCoerce (Refl :: Z :~: Z)
-"plusSuccDistrib" forall x y. plusSuccDistrib x y = unsafeCoerce (Refl :: Z :~: Z)
+"plusAssoc" plusAssoc = \_ _ _ -> unsafeCoerce (Refl :: Z :~: Z)
+"plusZeroNeutral" plusZeroNeutral = \_ -> unsafeCoerce (Refl :: Z :~: Z)
+"plusSuccDistrib" plusSuccDistrib = \_ _ -> unsafeCoerce (Refl :: Z :~: Z)
+"plusComm" plusComm = \_ _ -> unsafeCoerce (Refl :: Z :~: Z)
  #-}
